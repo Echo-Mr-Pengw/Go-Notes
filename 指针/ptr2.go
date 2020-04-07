85fe068100a532f5528c5eb6ca116296dd5e597d
pengw <2943927631@qq.com>
2020-04-07 19:55:11 +0800

    指针例子

---
+++ 指针/ptr2.go
@@ -0,0 +1,21 @@
+//指针作为参数传递
+
+package main
+
+import "fmt"
+
+//修改局部变量i的方法
+func ptrParams(ptr *int) int {
+	*ptr = *ptr + 10
+	return *ptr
+}
+
+func main() {
+
+	//声明变量
+	i := 10
+
+	//指针作为参数传递
+	res := ptrParams(&i);
+	fmt.Println(i, res);   //output: 20  20
+}
