87a96ade3629f2f3794a942393b2680ea2d0fe98
pengw <2943927631@qq.com>
2020-04-08 17:34:21 +0800

    goto 语句

---
+++ 循环语句/goto.go
@@ -0,0 +1,24 @@
+//goto 跳转到需要执行的语句
+
+package main 
+
+import "fmt"
+
+func main() {
+
+	i := 1
+
+	if i > 0 {
+		goto lab1  //跳转到lab1
+	}
+
+	fmt.Println(1);
+	fmt.Println(2);
+	
+	lab1:
+	//执行
+	fmt.Println(11);
+	fmt.Println(22);
+
+	//output: 11 22
+}
