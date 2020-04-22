ed97713891b90694af121998678d4573f762f2f0
pengw <2943927631@qq.com>
2020-04-22 11:36:47 +0800

    循环章节 注释统一化

--- 循环语句/example/for.go
+++ 循环语句/example/for.go
@@ -2,23 +2,25 @@
 
 package main 
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
-	//for循环语句的第一种写法
+	// for循环语句的第一种写法
 	for i := 1; i < 10; i++ {
 		fmt.Println(i)   //output： 1 2 3 4 5 6 7 8 9
 	}
 
-	//for循环语句的第二种写法
+	// for循环语句的第二种写法
 	j := 1
 	for j < 10 {
 		fmt.Println(j)   //output： 1 2 3 4 5 6 7 8 9
 		j++
 	}
 
-	//for循环语句的第三种写法
+	// for循环语句的第三种写法
 	k := 1
 	for true {
 		if k < 10 {
--- 循环语句/example/goto.go
+++ 循环语句/example/goto.go
@@ -2,7 +2,9 @@
 
 package main 
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 
