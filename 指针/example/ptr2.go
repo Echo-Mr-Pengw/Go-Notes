06d2783f985a0f90830a709c3e8959b2a7b6b861
pengw <2943927631@qq.com>
2020-04-26 19:40:19 +0800

    指针的代码整理

--- 指针/example/ptr2.go
+++ 指针/example/ptr2.go
@@ -1,10 +1,12 @@
-//指针作为参数传递
+// 指针作为参数传递
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-//修改局部变量i的方法
+// 修改局部变量i的方法
 func ptrParams(ptr *int) int {
 	*ptr = *ptr + 10
 	return *ptr
@@ -12,10 +14,10 @@ func ptrParams(ptr *int) int {
 
 func main() {
 
-	//声明变量
+	// 声明变量
 	i := 10
 
-	//指针作为参数传递
+	// 指针作为参数传递
 	res := ptrParams(&i);
 	fmt.Println(i, res);   //output: 20  20
 }
